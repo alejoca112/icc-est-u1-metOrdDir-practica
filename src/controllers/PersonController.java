15bbe406358a46db6087e9d2780e042673683ccf
Geovanny Cabrera <cabrerageovanny03@gmail.com>
2025-11-11 18:58:17 +0000

    Practica Examen

--- src/controllers/PersonController.java
+++ src/controllers/PersonController.java
@@ -7,25 +7,20 @@ public class PersonController {
     public void sortByName(Person[] personas) {
         for (int i = 0; i < personas.length; i++) {
             boolean swapped = false;
-            for (int j = 0; j < personas.length - i - 1; j++) {
-                String a = personas[j].getName();
-                String b = personas[j + 1].getName();
-                if (a == null)
-                    a = "";
-                if (b == null)
-                    b = "";
-                int cmp = a.compareToIgnoreCase(b);
-                if (cmp > 0 || (cmp == 0 && a.compareTo(b) > 0)) {
+            for (int j = 0; j < personas.length - 1 - i; j++) {
+                if (personas[j].getName().compareTo(personas[j+1].getName()) > 0) {
                     // Intercambio Correspondiente
-                    Person tmp = personas[j];
+                    Person temp = personas[j];
                     personas[j] = personas[j + 1];
-                    personas[j + 1] = tmp;
+                    personas[j + 1] = temp;
                     swapped = true;
                 }
             }
-            if (!swapped)
+            if (!swapped) {
+                //algo se hace
                 break;
-            // algo se hace
+            }
+            
 
         }
     }
